2ba1af6be56b8428d9864ad343b11754010cb0c6
Nguyen Cao Bach <nguyencaobach1010@gmail.com>
2023-10-12 14:38:18 +0700

    first commit

---
+++ src/main/java/com/bachnc/springbootrestfultemplate/SpringbootRestfulTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.bachnc.springbootrestfultemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestfulTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bachnc/springbootrestfultemplate/SpringbootRestfulTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachnc.springbootrestfultemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
